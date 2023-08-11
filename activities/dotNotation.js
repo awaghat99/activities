d16e6bbf0c17ed7c4a42b2ef9a2ea944fc1f5b25
awaghat99 <awaghat99@gmail.com>
2023-08-11 10:48:26 +0100

    add dot notation activity

---
+++ activities/dotNotation.js
@@ -0,0 +1,26 @@
+// ACTIVITY 
+
+console.log("   |  |  ")
+console.log("   |  |  ")
+console.log("   |  |  ")
+console.log("-----------")
+console.log("   |  |  ")
+console.log("   |  |  ")
+console.log("   |  |  ")
+console.log("-----------")
+console.log("   |  |  ")
+console.log("   |  |  ")
+console.log("   |  |  ")
+
+// STRETCH
+console.log("\n\n")
+for (let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+        console.log("   |  |  ")
+    }
+    if (i === 2) {
+        break
+    } else {
+        console.log("-----------")
+    }
+}
